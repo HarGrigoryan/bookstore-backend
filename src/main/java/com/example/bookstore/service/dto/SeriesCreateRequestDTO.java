64d688a731ab93e3f76f199f600a77b3f3c32c38
HarGrigoryan <harut.grigoryan0405@gmail.com>
2025-05-07 21:27:11 +0400

    Implement SeriesCreateRequestDTO

---
+++ src/main/java/com/example/bookstore/service/dto/SeriesCreateRequestDTO.java
@@ -0,0 +1,18 @@
+package com.example.bookstore.service.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class SeriesCreateRequestDTO {
+
+    @NotBlank
+    private String name;
+
+}
