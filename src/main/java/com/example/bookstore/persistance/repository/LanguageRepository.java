c21793a2af9e2a4e5f59a7b398573de16a23922b
HarGrigoryan <harut.grigoryan0405@gmail.com>
2025-05-07 20:30:39 +0400

    Added queries to LanguageRepository
    Added - `dependentBookIds(Long)`
    Added - `findByLanguage(String)`

--- src/main/java/com/example/bookstore/persistance/repository/LanguageRepository.java
+++ src/main/java/com/example/bookstore/persistance/repository/LanguageRepository.java
@@ -1,9 +1,22 @@
 package com.example.bookstore.persistance.repository;
 
 import com.example.bookstore.persistance.entity.Language;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LanguageRepository extends JpaRepository<Language, Long> {
+    @Query("""
+            SELECT b.id FROM Language l INNER JOIN Book b ON l = b.language WHERE l.id = :id
+            """)
+    List<Long> dependentBookIds(Long id);
+
+    @Query("""
+        SELECT l FROM Language l WHERE l.language = :name
+        """)
+    Language findByLanguage(@NotBlank String name);
 }
