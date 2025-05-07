a8b4d50403dddd508adf8c63bb1a6715a169a99c
HarGrigoryan <harut.grigoryan0405@gmail.com>
2025-05-07 21:09:42 +0400

    Implement Exception handling classes
    Implemented an exception class for better error representation. Also, implemented a ExceptionResponse class for graceful error handling with HTTP requests. Lastly implemented a 'RestControllerAdvice' class for proper exception handling with Security

---
+++ src/main/java/com/example/bookstore/exception/GlobalExceptionHandler.java
@@ -0,0 +1,116 @@
+package com.example.bookstore.exception;
+
+import jakarta.servlet.ServletException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidTokenException.class)
+    public ResponseEntity<ExceptionResponse> handleInvalidTokenException(InvalidTokenException e) {
+        final ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+                .withMessage(e.getMessage())
+                .withStatus(HttpStatus.BAD_REQUEST)
+                .build();
+        return ResponseEntity.status(exceptionResponse.getStatus()).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(EntityAlreadyExistsException.class)
+    public ResponseEntity<ExceptionResponse> handleEntityAlreadyExistsException(EntityAlreadyExistsException e) {
+        final ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+                .withMessage(e.getMessage())
+                .withStatus(HttpStatus.BAD_REQUEST)
+                .build();
+        return ResponseEntity.status(exceptionResponse.getStatus()).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ExceptionResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        final ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+                .withMessage(e.getMessage())
+                .withStatus(HttpStatus.BAD_REQUEST)
+                .build();
+        return ResponseEntity.status(exceptionResponse.getStatus()).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ExceptionResponse> handleEntityNotFoundException(EntityNotFoundException e) {
+        final ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+                .withMessage(e.getMessage())
+                .withStatus(HttpStatus.NOT_FOUND)
+                .build();
+        return ResponseEntity.status(exceptionResponse.getStatus()).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(EntityDeletionException.class)
+    public ResponseEntity<ExceptionResponse> handleEntityDeletionException(EntityDeletionException e) {
+        final ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+                .withMessage(e.getMessage())
+                .withStatus(HttpStatus.CONFLICT)
+                .build();
+        return ResponseEntity.status(exceptionResponse.getStatus()).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(ResourceAlreadyUsedException.class)
+    public ResponseEntity<ExceptionResponse> handleResourceAlreadyUsedException(ResourceAlreadyUsedException e) {
+        final ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+                .withMessage(e.getMessage())
+                .withStatus(HttpStatus.CONFLICT)
+                .build();
+        return ResponseEntity.status(exceptionResponse.getStatus()).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        final ExceptionResponse exceptionResponse = this.getBadRequestResponseBuilder(ex.getBindingResult())
+                .build();
+        return ResponseEntity.status(exceptionResponse.getStatus()).body(exceptionResponse);
+    }
+
+    private ExceptionResponse.ExceptionResponseBuilder getBadRequestResponseBuilder(BindingResult result) {
+        final String message = result.getFieldErrors().stream()
+                .findFirst()
+                .map(fieldError -> String.join(" ", StringUtils.capitalize(fieldError.getField()), fieldError.getDefaultMessage()))
+                .orElse(null);
+        return ExceptionResponse.builder()
+                .withMessage(message)
+                .withStatus(HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ServletException.class)
+    public ResponseEntity<ExceptionResponse> handleException(ServletException e) {
+        final ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+                .withMessage(e.getMessage())
+                .withStatus(HttpStatus.BAD_REQUEST)
+                .build();
+        return ResponseEntity.status(exceptionResponse.getStatus()).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        final ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+                .withMessage(e.getMessage())
+                .withStatus(HttpStatus.BAD_REQUEST)
+                .build();
+        return ResponseEntity.status(exceptionResponse.getStatus()).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ExceptionResponse> handleRuntime(RuntimeException ex) {
+        ExceptionResponse resp = ExceptionResponse.builder()
+                .withMessage("Unexpected error: " + ex.getMessage())
+                .withStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                .build();
+        return ResponseEntity.status(resp.getStatus()).body(resp);
+    }
+
+
+}
