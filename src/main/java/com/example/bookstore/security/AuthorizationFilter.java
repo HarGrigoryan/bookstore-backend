11f630b0722c9da56d44f9471dc1334628dac5a4
HarGrigoryan <harut.grigoryan0405@gmail.com>
2025-05-07 20:12:26 +0400

    Implement JwtUtil and AuthorizationFilter
    Implemented a Once Per Request Filter and JwtUtil classes to implement a JWT authorization

---
+++ src/main/java/com/example/bookstore/security/AuthorizationFilter.java
@@ -0,0 +1,60 @@
+package com.example.bookstore.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import com.example.bookstore.security.util.JwtUtil;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.example.bookstore.security.util.JwtUtil.AUTH_TYPE;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@RequiredArgsConstructor
+public class AuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        final String jwtToken = request.getHeader(AUTHORIZATION);
+        if (jwtToken == null || !jwtToken.startsWith(AUTH_TYPE)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String token = jwtToken.substring(AUTH_TYPE.length()).trim();
+        if (!jwtUtil.isVerified(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String username = jwtUtil.getUsername(token);
+        final String[] authorities = jwtUtil.getAuthorities(token);
+
+        final List<SimpleGrantedAuthority> grantedAuthorities = Arrays.stream(authorities)
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+
+        final UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(username, null, grantedAuthorities);
+
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        filterChain.doFilter(request, response);
+    }
+}
