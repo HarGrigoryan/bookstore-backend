a86d0f4223c69d8cb35d2ed3c8167597e5ae7727
HarGrigoryan <harut.grigoryan0405@gmail.com>
2025-05-16 01:51:50 +0400

    Improve BookInstanceController with proper security

--- src/main/java/com/example/bookstore/controller/BookInstanceController.java
+++ src/main/java/com/example/bookstore/controller/BookInstanceController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -20,43 +21,48 @@ import java.math.BigDecimal;
 public class BookInstanceController {
 
     private final BookInstanceService bookInstanceService;
-    //TODO: Proper security
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('USER', 'STAFF')")
     public ResponseEntity<BookInstanceDTO> getById(@PathVariable Long id) {
         return ResponseEntity.ok(bookInstanceService.getById(id));
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('STAFF')")
     public PageResponseDTO<BookInstanceDTO> getAll(BookInstanceSearchCriteria criteria) {
         return bookInstanceService.getAll(criteria);
     }
 
     @GetMapping("/{id}/renting-cost")
+    @PreAuthorize("hasAnyRole('USER', 'STAFF')")
     public ResponseEntity<BigDecimal> getRentingCost(@PathVariable Long id) {
         return ResponseEntity.ok(bookInstanceService.getRentingCost(id));
     }
 
     @GetMapping("/{id}/sale-cost")
+    @PreAuthorize("hasAnyRole('USER', 'STAFF')")
     public ResponseEntity<BigDecimal> getSaleCost(@PathVariable Long id) {
         return ResponseEntity.ok(bookInstanceService.getSaleCost(id));
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('MANAGER') OR hasPermission('ROLE_STAFF', 'ADD_BOOK_INSTANCE')")
     public ResponseEntity<BookInstanceDTO> create(@RequestBody @Valid BookInstanceRequestDTO bookInstanceRequestDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(bookInstanceService.create(bookInstanceRequestDTO));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('STAFF')")
     public ResponseEntity<BookInstanceDTO> update(@PathVariable Long id, @RequestBody @Valid BookInstanceRequestDTO bookInstanceRequestDTO) {
         return ResponseEntity.ok(bookInstanceService.updateById(id, bookInstanceRequestDTO));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('MANAGER') OR hasPermission('ROLE_STAFF', 'REMOVE_INFORMATION')")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         bookInstanceService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
-
 
 }
