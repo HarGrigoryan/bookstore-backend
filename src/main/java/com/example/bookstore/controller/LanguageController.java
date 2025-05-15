ece121e9cda705584bad644a371fda4b22f0e1ab
HarGrigoryan <harut.grigoryan0405@gmail.com>
2025-05-16 01:58:05 +0400

    Improve LanguageController with proper security

--- src/main/java/com/example/bookstore/controller/LanguageController.java
+++ src/main/java/com/example/bookstore/controller/LanguageController.java
@@ -19,31 +19,33 @@ public class LanguageController {
     private final LanguageService languageService;
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('USER', 'STAFF')")
     public LanguageDTO getById(@PathVariable Long id) {
         return languageService.getById(id);
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('USER', 'STAFF')")
     public List<LanguageDTO> getAll() {
         return languageService.getAll();
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PreAuthorize("hasAuthority('ROLE_MANAGER')")
+    @PreAuthorize("hasRole('ROLE_MANAGER') OR hasPermission('ROLE_STAFF', 'REMOVE_INFORMATION')")
     public void deleteById(@PathVariable Long id) {
         languageService.delete(id);
     }
 
     @PutMapping("/{id}")
-    @PreAuthorize("hasAuthority('ROLE_STAFF')")
+    @PreAuthorize("hasRole('ROLE_STAFF')")
     public LanguageDTO updateById(@PathVariable Long id, @RequestBody @Valid LanguageRequestDTO languageDTO) {
         return languageService.updateById(id, languageDTO);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @PreAuthorize("hasAuthority('ROLE_STAFF')")
+    @PreAuthorize("hasRole('ROLE_MANAGER') OR hasPermission('ROLE_STAFF', 'ADD_INFORMATION')")
     public LanguageDTO create(@RequestBody @Valid LanguageRequestDTO languageDTO) {
         return languageService.create(languageDTO);
     }
